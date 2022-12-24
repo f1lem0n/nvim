46184549c91b33360ac3e7d4b7115b7e3a18b327
Filip Hajdyła <f1lem0n@deb-laptop.deb-laptop>
2022-12-24 14:00:18 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt install -y  \
-    curl
-    neovim
-    yarn
+    curl             \
+    neovim           \
+    yarn             \
     silversearcher-ag
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
