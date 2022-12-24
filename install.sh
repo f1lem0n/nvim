09077e13809b558a233ae4dbfe5668b597e9f317
Filip Hajdyła <f1lem0n@deb-laptop.deb-laptop>
2022-12-24 14:17:55 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt install -y  \
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
     --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-nvim README.md -c PlugUpgrade q PlugInstall q PlugUpdate q 2> install.err
+nvim README.md -c PlugUpgrade q PlugInstall q PlugUpdate q
 
 if [ -n install.err ]; then
     cat install.err
