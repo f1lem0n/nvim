4f8b8a58474fec36779bd2c85819b213d4d8adeb
Filip Hajdyła <hajdylaf@gmail.com>
2022-12-24 14:31:52 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,3 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
     --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim README.md -c PlugInstall
-
-if [ -n install.err ]; then
-    cat install.err
-else:
-    rm install.err
-fi
